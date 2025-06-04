971ca24aaff80f39c09825449d3bc216e6e35e10
arimura <33759663+arimura@users.noreply.github.com>
2025-06-04 14:44:07 +0900

    Add xml activity

--- AdSdkDemo/app/build.gradle.kts
+++ AdSdkDemo/app/build.gradle.kts
@@ -51,6 +51,10 @@ dependencies {
     implementation(libs.androidx.material3)
     // Google Mobile Ads SDK
     implementation("com.google.android.gms:play-services-ads:24.3.0")
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
